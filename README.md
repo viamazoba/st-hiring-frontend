94e70cf9684da41ff9cebeadf3ecee42b52e278a
Daniel Reyes <daniel@dahulabs.com>
2024-06-26 12:08:47 -0500

    Initial files

---
+++ README.md
@@ -0,0 +1,3 @@
+# See Tickets Frontend challenge
+
+This project is a React application designed for a technical interview. The main objective is to create a user interface that interacts with a Node.js backend through various endpoints. The application will demonstrate the use of state management, forms, and UI frameworks such as Material-UI (MUI).
---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1>See Tickets </h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
